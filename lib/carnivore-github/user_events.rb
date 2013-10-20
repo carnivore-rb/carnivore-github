1041c61bc659ca1e9c08c103b5d3546a686031b8
Chris Roberts <chrisroberts.code@gmail.com>
2013-10-20 09:14:09 -0700

    Setup timer loop and use queue for message passing

--- lib/carnivore-github/user_events.rb
+++ lib/carnivore-github/user_events.rb
@@ -1,3 +1,4 @@
+require 'thread'
 require 'carnivore-github/github'
 
 module Carnivore
@@ -5,12 +6,44 @@ module Carnivore
     class GithubUserEvents < Github
 
       attr_reader :last_id
+      attr_reader :queue
+
+      def setup(args={})
+        @queue = Queue.new
+        super
+      end
+
+      def terminate
+        super
+        if(timer)
+          timer.cancel
+        end
+      end
 
       def connect
         init = fetch(url(events_path), false)
         update_settings(init)
         @last_id = init.parse_body.first['id']
         debug "Initial last id set: #{last_id}"
+        @timer = after(poll.to_i) do
+          result = fetch(url(events_path))
+          if(result.status == 200)
+            body = result.parse_body
+            messages = body
+            last_idx = messages.index{|m| m['id'] == last_id}
+            if(last_idx)
+              messages = messages.slice(0, last_idx)
+              unless(messages.empty?)
+                @last_id = messages.first['id']
+                debug "Last id updated to: #{last_id}"
+              end
+              messages.each do |m|
+                queue << m
+              end
+            end
+            update_settings(result)
+          end
+        end
       end
 
       def events_path
@@ -22,27 +55,7 @@ module Carnivore
       end
 
       def receive(*args)
-        messages = []
-        timer.after(poll.to_i) do
-          result = fetch(url(events_path))
-          if(result.status == 200)
-            body = result.parse_body
-            messages = body
-            last_idx = messages.index{|m| m['id'] == last_id}
-            if(last_idx)
-              messages = messages.slice(0, last_idx)
-            end
-            unless(messages.empty?)
-              @last_id = messages.first['id']
-              debug "*" * 20
-              debug "Last id updated to: #{last_id}"
-              debug "*" * 20
-            end
-            update_settings(result)
-          end
-        end
-        timer.wait
-        messages
+        queue.pop
       end
 
     end
